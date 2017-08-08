77534ec3417893d38bae7e1ad4d5d68576107cac
Opara Nnabueze <villavelle101@gmail.com>
2017-08-08 05:25:51 +0100

    added toolbar with home menu

--- app/src/main/java/com/example/eze/materialtest/SubActivity.java
+++ app/src/main/java/com/example/eze/materialtest/SubActivity.java
@@ -1,23 +1,30 @@
 package com.example.eze.materialtest;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
+public class SubActivity extends AppCompatActivity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
 
         toolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
@@ -35,9 +42,10 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Menu Settings Selected", Toast.LENGTH_SHORT).show();
         }
 
-        if (id == R.id.navigate){
-            startActivity(new Intent(this, SubActivity.class));
+        if (id == android.R.id.home){
+            startActivity(new Intent(this, MainActivity.class));
         }
         return true;
     }
+
 }
