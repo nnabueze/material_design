78d15fa48cf1dc45dbcfa36e8c75eda319ef3bfe
Opara Nnabueze <villavelle101@gmail.com>
2017-08-08 02:50:01 +0100

    menu

--- app/src/main/java/com/example/eze/materialtest/MainActivity.java
+++ app/src/main/java/com/example/eze/materialtest/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.eze.materialtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -13,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflate = getMenuInflater();
+        inflate.inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.setting){
+            Toast.makeText(this, "Menu Settings Selected", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
